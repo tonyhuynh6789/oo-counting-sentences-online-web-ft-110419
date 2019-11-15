43cf2d85a12c3b4aa992f0d11d413fa827e5d6ed
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-15 05:44:32 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,8 @@ class String
   end
 
   def count_sentences
-  new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+  new_array = self.split(/[.?!]/)
+  new_array.reject {|string| string.empty?}
   return new_array.length
   end
   
